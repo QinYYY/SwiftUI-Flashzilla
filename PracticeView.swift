070bd8bc5fc6ce1407d2e56d29d4ec66489069d9
QinYYY <qinyang_c@outlook.com>
2024-09-10 15:11:42 +0930

    Gesture

---
+++ PracticeView.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeView.swift
+//  Flashzilla
+//
+//  Created by QinY on 10/9/2024.
+//
+
+import SwiftUI
+
+struct PracticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PracticeView()
+}
